b8f73dd48173f9bb094959f1b8631c8d2d6f23fb
supercurses <russell.ashby@gmail.com>
2024-10-10 12:03:03 +0100

    Adding error handling to API calls

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from agent_review import ReviewAgent
 from agent_sdxl import SDXLAgent
 from tokenizer import Tokenizer
 
-
 app.add_static_files('/images', 'images')
+
 
 async def update_timer(label, start_time):
     """ Starts a timer and updates the given label with elapsed time"""
@@ -78,24 +78,28 @@ async def generate_image():
     with carousel_placeholder:
         spinner = ui.spinner(size='xl')
         spinner.visible = True
+    try:
+        # Start the processing timer
+        start_time = time.time()
+        timer_task = asyncio.create_task((update_timer(stopwatch_label, start_time)))
+        urls = await run.io_bound(flux_agent.generate_image, model="black-forest-labs/flux-schnell",
+                                  prompt=prompt, steps=4, controlnet=False, image_url=None)
+    except Exception as e:
+        ui.notify(f'Unable to get an image: {str(e)}', type='negative')
+    finally:
+        timer_task.cancel()
+        flux_generate_button.enable()
+        # End the timer
+        end_time = time.time()
+        url = urls[0]
+        flux_image_urls.append(url)
+        review_button.style('visibility: visible')
+        sdxl_button.style('visibility: visible')
+        flux_button.style('visibility: visible')
+        spinner.visible = False
+        stopwatch_label.set_text(f"Final time: {time.time() - start_time:.2f} seconds")
+        update_carousel()
 
-    # Start the processing timer
-    start_time = time.time()
-    timer_task = asyncio.create_task((update_timer(stopwatch_label,start_time)))
-    urls = await run.io_bound(flux_agent.generate_image, model="black-forest-labs/flux-schnell",
-                              prompt=prompt, steps=4, controlnet=False, image_url=None)
-    timer_task.cancel()
-    flux_generate_button.enable()
-    # End the timer
-    end_time = time.time()
-    url = urls[0]
-    flux_image_urls.append(url)
-    review_button.style('visibility: visible')
-    sdxl_button.style('visibility: visible')
-    flux_button.style('visibility: visible')
-    spinner.visible = False
-    stopwatch_label.set_text(f"Final time: {time.time() - start_time:.2f} seconds")
-    update_carousel()
 
 async def generate_image_flux_dev():
     """ Creates a flux dev image from the prompt and uses the image as a controlnet """
@@ -104,17 +108,21 @@ async def generate_image_flux_dev():
     with carousel_placeholder:
         spinner = ui.spinner(size='xl')
         spinner.visible = True
-    urls = await run.io_bound(flux_agent.generate_image,
-                              model="xlabs-ai/flux-dev-controlnet"
-                                    ":f2c31c31d81278a91b2447a304dae654c64a5d5a70340fba811bb1cbd41019a2",
-                              prompt=prompt, steps=28,
-                              controlnet=True, image_url=control_url)
-    url = urls[0]
-    #flux_image.source = url
-    #flux_image_label.set_text(url)
-    review_button.style('visibility: visible')
-    sdxl_button.style('visibility: visible')
-    spinner.visible = False
+    try:
+        urls = await run.io_bound(flux_agent.generate_image,
+                                  model="xlabs-ai/flux-dev-controlnet"
+                                        ":f2c31c31d81278a91b2447a304dae654c64a5d5a70340fba811bb1cbd41019a2",
+                                  prompt=prompt, steps=28,
+                                  controlnet=True, image_url=control_url)
+    except Exception as e:
+        ui.notify(f'Unable to get an image: {str(e)}', type='negative')
+    finally:
+        url = urls[0]
+        flux_image_urls.append(url)
+        review_button.style('visibility: visible')
+        sdxl_button.style('visibility: visible')
+        spinner.visible = False
+        update_carousel()
 
 
 async def review_image():
@@ -122,11 +130,17 @@ async def review_image():
     review_dialog.open()
     with review_row:
         review_spinner = ui.spinner('dots', size='xl')
-    review.content = await run.io_bound(review_agent.review_image, url=flux_image_label.text)
-    review_spinner.visible = False
+    try:
+        review.content = await run.io_bound(review_agent.review_image, url=flux_image_label.text)
+    except Exception as e:
+        ui.notify(f'Unable to generate a review: {str(e)}', type='negative')
+    finally:
+        review_spinner.visible = False
+
 
 def sdxl_dialog_manager():
     sdxl_dialog.open()
+
 
 def update_carousel():
     carousel_placeholder.clear()
@@ -155,21 +169,24 @@ def set_current_image():
     sdxl_image.set_source(current_url)
 
 
-
-
 async def generate_sdxl():
     """ Uses a local Automatic 1111 installation to create an SDXL image to image rendering """
     # Start the processing timer
     start_time = time.time()
     timer_task = asyncio.create_task((update_timer(sdxl_stopwatch_label, start_time)))
-    file_path = await run.io_bound(sdxl_agent.img2img, img2img_prompt=prompt_textarea.value,
-                       counter=1,
-                       image_path="nicegui_img2img.png",
-                       image_url=flux_image_label.text,
-                       adetailer=True
-                       )
-    timer_task.cancel()
-    sdxl_image.source = file_path
+    try:
+        file_path = await run.io_bound(sdxl_agent.img2img, img2img_prompt=prompt_textarea.value,
+                                       counter=1,
+                                       image_path="nicegui_img2img.png",
+                                       image_url=flux_image_label.text,
+                                       adetailer=True
+                                       )
+    except Exception as e:
+        ui.notify(f'Unable to get an image: {str(e)}', type='negative')
+    finally:
+        timer_task.cancel()
+        sdxl_image.source = file_path
+
 
 prompts = []
 flux_agent = FluxAgent()
@@ -180,7 +197,6 @@ tokenizer = Tokenizer()
 styles = {1: 'photograph', 2: 'illustration', 3: 'oil painting'}
 flux_image_urls = []
 
-
 with ui.row().style('gap:10em').classes('w-full no-wrap'):
     with ui.column().classes('w-1/2 pl-20 pt-10'):
         # Create the input field and button
@@ -188,20 +204,21 @@ with ui.row().style('gap:10em').classes('w-full no-wrap'):
             'width:75%')  # Store the input field in a variable for later access
         ui.button('get a prompt', on_click=generate_prompts)
         prompt_textarea = ui.textarea('Embellished Prompt',
-                                      on_change=lambda e: update_sequence_length()).props('autogrow').style('width:75%;')
+                                      on_change=lambda e: update_sequence_length()).props('autogrow').style(
+            'width:75%;')
 
         sequence_length = ui.label('Sequence Length')
         with ui.row():
             ui.button('shrink prompt', on_click=shrink_prompt)
             flux_generate_button = ui.button('generate image', on_click=generate_image)
 
-
     with ui.column().classes('w-1/2 pr-20 pt-10'):
         stopwatch_label = ui.label()
-        #flux_image = ui.image().style('width: 100%; height: 60%; background-color: silver')
-        #flux_image.source = 'images/placeholder.png'
+        # flux_image = ui.image().style('width: 100%; height: 60%; background-color: silver')
+        # flux_image.source = 'images/placeholder.png'
         flux_image_label = ui.label().style('visibility: hidden')
-        with ui.carousel(animated=True, arrows=True, navigation=True, on_value_change=set_current_image).props("height=600px") as carousel_placeholder:
+        with ui.carousel(animated=True, arrows=True, navigation=True, on_value_change=set_current_image).props(
+                "height=600px") as carousel_placeholder:
             with ui.carousel_slide():
                 placeholder_image = ui.image('images/placeholder.png').classes('w-[600px] h-[600px]')
 
@@ -209,7 +226,6 @@ with ui.row().style('gap:10em').classes('w-full no-wrap'):
             review_button = ui.button('rate image', on_click=review_image).style('visibility: hidden')
             sdxl_button = ui.button('SDXL', on_click=sdxl_dialog_manager).style('visibility: hidden')
             flux_button = ui.button('send to flux dev', on_click=generate_image_flux_dev).style('visibility: hidden')
-
 
 with ui.dialog() as review_dialog, ui.card().style('width:50%; max-width: none') as review_card:
     with ui.row() as review_row:
@@ -224,6 +240,5 @@ with ui.dialog() as sdxl_dialog, ui.card().style('width:50%; max-width:none'):
     ui.button('Get CLIP', on_click=generate_clip_prompt).style('visibility: visible')
     sdxl_image = ui.image().style('height: 60%')
     sdxl_go_button = ui.button('go', on_click=generate_sdxl).style('visibility: visible')
-
 
 ui.run()
